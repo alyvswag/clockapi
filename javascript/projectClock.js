5023177d2f2963d4e0eb699e79eaf41cff01fede
Talib Aliyev <133005678+alyvswag@users.noreply.github.com>
2024-08-08 17:38:26 +0400

    YeniDuzelislerEdildi

--- javascript/projectClock.js
+++ javascript/projectClock.js
@@ -43,9 +43,9 @@ setInterval(clock, 1000); //her kecen saniyenin gorunmesi ucun SetInterval metod
 navigator.geolocation.getCurrentPosition(function (position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-    const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+    const geoApiUrl1 = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
     // console.log(geoApiUrl);
-    fetch(geoApiUrl)
+    fetch(geoApiUrl1)
         .then(response => response.json())
         .then(data => {
             const city = data.city;
@@ -63,7 +63,7 @@ navigator.geolocation.getCurrentPosition(function (position) {
             document.querySelector(".havaDerece").innerHTML = tamEdedHavaderece + "°C";
             // console.log(weatherCelsius);
             if (tamEdedHavaderece >= 10 && tamEdedHavaderece <= 15) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-sun-cloud"></i>`;
+                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-cloud-sun"></i>`;
             }
             else if (tamEdedHavaderece > 15 && tamEdedHavaderece <= 25) {
                 document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-sun"></i>`;
@@ -96,101 +96,151 @@ navigator.geolocation.getCurrentPosition(function (position) {
 
 });
 
-let cities = ['New York', 'Istanbul', 'Paris', 'Rome', 'Moscow', 'Sydney'];
-let randomIndex = Math.floor(Math.random() * cities.length);
-let randomCity = cities[randomIndex];
-let randomIndex2;
-let randomCity2;
-do {
-    randomIndex2 = Math.floor(Math.random() * cities.length);
-    randomCity2 = cities[randomIndex2];
-} while (randomIndex === randomIndex2);
+// let cities = ['New York', 'Istanbul', 'Paris', 'Rome', 'Moscow', 'Sydney'];
+// let randomIndex = Math.floor(Math.random() * cities.length);
+// let randomCity = cities[randomIndex];
+// let randomIndex2;
+// let randomCity2;
+// do {
+//     randomIndex2 = Math.floor(Math.random() * cities.length);
+//     randomCity2 = cities[randomIndex2];
+// } while (randomIndex === randomIndex2);
 
+// var cityLatLut = [
+//     [40.730610, -73.935242],//newyork
+//     [41.015137, 28.979530],//istanbul
+//     [48.864716, 2.349014],//paris
+//     [41.902782, 12.496366],//roma
+//     [55.751244, 37.618423],//moscow
+//     [-33.865143, 151.209900]//sidney
+// ];
+// function getRandomCities() {
+//     let cities = ['New York', 'Istanbul', 'Paris', 'Rome', 'Moscow', 'Sydney'];
+//     let randomIndex = Math.floor(Math.random() * cities.length);
+//     let randomCity = cities[randomIndex];
+//     let randomIndex2;
+//     let randomCity2;
+//     do {
+//         randomIndex2 = Math.floor(Math.random() * cities.length);
+//         randomCity2 = cities[randomIndex2];
+//     } while (randomIndex === randomIndex2);
+
+//     var cityLatLut = [
+//         [40.730610, -73.935242], // New York
+//         [41.015137, 28.979530],  // Istanbul
+//         [48.864716, 2.349014],   // Paris
+//         [41.902782, 12.496366],  // Rome
+//         [55.751244, 37.618423],  // Moscow
+//         [-33.865143, 151.209900] // Sydney
+//     ];
+
+//     return {
+//         city1: { name: randomCity, coordinates: cityLatLut[randomIndex] },
+//         city2: { name: randomCity2, coordinates: cityLatLut[randomIndex2] }
+//     };
+// }
+
+// Kullanımı:
+// const { city1, city2 } = getRandomCities();
 var cityLatLut = [
-    [40.730610, -73.935242],//newyork
-    [41.015137, 28.979530],//istanbul
-    [48.864716, 2.349014],//paris
-    [41.902782, 12.496366],//roma
-    [55.751244, 37.618423],//moscow
-    [-33.865143, 151.209900]//sidney
+    [40.730610, -73.935242], // New York
+    [41.015137, 28.979530],  // Istanbul
+    [48.864716, 2.349014],   // Paris
+    [41.902782, 12.496366],  // Rome
+    [55.751244, 37.618423],  // Moscow
+    [-33.865143, 151.209900] // Sydney
 ];
+ 
 
 navigator.geolocation.getCurrentPosition(function (position) {
 
-    var latitude = cityLatLut[randomIndex][0];
-    var longitude = cityLatLut[randomIndex][1];
-    const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+    // var latitude = cityLatLut[randomIndex][0];
+    // var longitude = cityLatLut[randomIndex][1];
+    var latitude = cityLatLut[randomIndex1][0];
+    var longitude = cityLatLut[randomIndex1][1];
+    const geoApiUrl2 = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
     // console.log(geoApiUrl);
-    fetch(geoApiUrl)
+    fetch(geoApiUrl2)
         .then(response => response.json())
         .then(data => {
             const city = data.city;
             document.querySelector(".seherAdi").innerHTML = city;
         })
+        .catch(error => console.error('Error fetching timezone data:', error));
 
-    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=52d8c6f7cf31a177b158468840fef4da`;
+    const weatherApiUrl2 = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=52d8c6f7cf31a177b158468840fef4da`;
     // console.log(weatherApiUrl);
-    fetch(weatherApiUrl)
+    console.log(weatherApiUrl2);
+    fetch(weatherApiUrl2)
         .then(response => response.json())
         .then(data => {
             var weather = data.main.temp;
             var weatherCelsius = weather - 273.15;
             var tamEdedHavaderece = Math.round(weatherCelsius);
             document.querySelector(".ikinciHava").innerHTML = tamEdedHavaderece + "°C";
+            const unixTime = data.dt;
+            // Unix zaman damgasını milisaniyeye dönüştür
+            const currentTime = new Date(unixTime * 1000);
+            // Zaman dilimi bilgisini al ve istediğiniz formatta işleyin
+            const timeOptions = { hour: '2-digit', minute: '2-digit' };
+            const time = currentTime.toLocaleTimeString('en-US', timeOptions);
+            
+            // Zamanı istediğiniz şekilde kullanabilirsiniz
+            console.log('Zaman:', time);
             // console.log(weatherCelsius);
             if (tamEdedHavaderece >= 10 && tamEdedHavaderece <= 15) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-sun-cloud"></i>`;
+                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-cloud-sun"></i>`;
             }
             else if (tamEdedHavaderece > 15 && tamEdedHavaderece <= 25) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-sun"></i>`;
+                document.querySelector(".ikinciHavaFont1").innerHTML = `<i class="fa-solid fa-sun"></i>`;
 
             }
             else if (tamEdedHavaderece > 25) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-sun-bright"></i>`;
+                document.querySelector(".ikinciHavaFont1").innerHTML = `<i class="fa-solid fa-sun-bright"></i>`;
 
             }
             else if (tamEdedHavaderece < 10 && tamEdedHavaderece >= 5) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-cloud-sun"></i>`;
+                document.querySelector(".ikinciHavaFont1").innerHTML = `<i class="fa-solid fa-cloud-sun"></i>`;
 
             }
             else if (tamEdedHavaderece < 5 && tamEdedHavaderece >= 2) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-cloud"></i>`;
+                document.querySelector(".ikinciHavaFont1").innerHTML = `<i class="fa-solid fa-cloud"></i>`;
 
             }
             else if (tamEdedHavaderece < 2 && tamEdedHavaderece >= 0) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-clouds"></i>`;
+                document.querySelector(".ikinciHavaFont1").innerHTML = `<i class="fa-solid fa-cloud"></i>`;
 
             }
             else if (tamEdedHavaderece < 0 && tamEdedHavaderece >= -3) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-cloud-rain"></i>`;
+                document.querySelector(".ikinciHavaFont1").innerHTML = `<i class="fa-solid fa-cloud-rain"></i>`;
             }
 
             else if (tamEdedHavaderece < -3) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-snowflake"></i>`;
+                document.querySelector(".ikinciHavaFont1").innerHTML = `<i class="fa-solid fa-snowflake"></i>`;
             }
 
         })
 
-    function fetchTimeZoneData() {
-        timezoneKey = 'X6B3J2LJQON9';
-        const timeApiURl = `https://api.timezonedb.com/v2.1/get-time-zone?key=${timezoneKey}&format=json&by=position&lat=${latitude}&lng=${longitude}`;
-        setInterval(timeApiURl, 1000);
-        //console.log(timeApiURl);
-        fetch(timeApiURl)
-            .then(response => response.json())
-            .then(data => {
-                var clock = data.formatted;
-                var dataTimeParts = clock.split(" ");
-                var timePart = dataTimeParts[1].split(":");
-                var time = timePart[0] + ":" + timePart[1];
-                document.querySelector(".ikinciSaheSaatDiv1").innerHTML = timePart[0];
-                document.querySelector(".ikinciSaheSaatDiv3").innerHTML = timePart[1];
-                // console.log(time);
-            })
-
-    }
-    fetchTimeZoneData();
-    setInterval(fetchTimeZoneData, 1000);
+    // function fetchTimeZoneData() {
+    //    const timezoneKey = 'X6B3J2LJQON9';
+    //     const timeApiURl2 = `https://api.timezonedb.com/v2.1/get-time-zone?key=${timezoneKey}&format=json&by=position&lat=${latitude}&lng=${longitude}`;
+    //     setInterval(timeApiURl2, 1000);
+    //     //console.log(timeApiURl);
+    //     fetch(timeApiURl2)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             var clock = data.formatted;
+    //             var dataTimeParts = clock.split(" ");
+    //             var timePart = dataTimeParts[1].split(":");
+    //             var time = timePart[0] + ":" + timePart[1];
+    //             document.querySelector(".ikinciSaheSaatDiv1").innerHTML = timePart[0];
+    //             document.querySelector(".ikinciSaheSaatDiv3").innerHTML = timePart[1];
+    //             // console.log(time);
+    //         })
+    //         .catch(error => console.error('Error fetching timezone data:', error));
+    // }
+    // fetchTimeZoneData();
+    // setInterval(fetchTimeZoneData, 60000);
 
 });
 
@@ -199,18 +249,21 @@ navigator.geolocation.getCurrentPosition(function (position) {
 navigator.geolocation.getCurrentPosition(function (position) {
     var latitude = cityLatLut[randomIndex2][0];
     var longitude = cityLatLut[randomIndex2][1];
-    const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+    // var latitude = city2.coordinates;
+    // var longitude = city2.coordinates;
+    const geoApiUrl3 = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
     // console.log(geoApiUrl);
-    fetch(geoApiUrl)
+    fetch(geoApiUrl3)
         .then(response => response.json())
         .then(data => {
             const city = data.city;
             document.querySelector(".seherAdi2").innerHTML = city;
         })
+        .catch(error => console.error('Error fetching timezone data:', error));
 
-    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=52d8c6f7cf31a177b158468840fef4da`;
+    const weatherApiUrl3 = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=52d8c6f7cf31a177b158468840fef4da`;
     //  console.log(weatherApiUrl);
-    fetch(weatherApiUrl)
+    fetch(weatherApiUrl3)
         .then(response => response.json())
         .then(data => {
             var weather = data.main.temp;
@@ -219,42 +272,43 @@ navigator.geolocation.getCurrentPosition(function (position) {
             document.querySelector(".ikinciHava2").innerHTML = tamEdedHavaderece + "°C";
             //  console.log(weatherCelsius);
             if (tamEdedHavaderece >= 10 && tamEdedHavaderece <= 15) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-sun-cloud"></i>`;
+                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-cloud-sun"></i>`;
             }
             else if (tamEdedHavaderece > 15 && tamEdedHavaderece <= 25) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-sun"></i>`;
+                document.querySelector(".ikinciHavaFont2").innerHTML = `<i class="fa-solid fa-sun"></i>`;
 
             }
             else if (tamEdedHavaderece > 25) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-sun-bright"></i>`;
+                document.querySelector(".ikinciHavaFont2").innerHTML = `<i class="fa-solid fa-sun-bright"></i>`;
 
             }
             else if (tamEdedHavaderece < 10 && tamEdedHavaderece >= 5) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-cloud-sun"></i>`;
+                document.querySelector(".ikinciHavaFont2").innerHTML = `<i class="fa-solid fa-cloud-sun"></i>`;
 
             }
             else if (tamEdedHavaderece < 5 && tamEdedHavaderece >= 2) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-cloud"></i>`;
+                document.querySelector(".ikinciHavaFont2").innerHTML = `<i class="fa-solid fa-cloud"></i>`;
 
             }
             else if (tamEdedHavaderece < 2 && tamEdedHavaderece >= 0) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-clouds"></i>`;
+                document.querySelector(".ikinciHavaFont2").innerHTML = `<i class="fa-solid fa-cloud"></i>`;
 
             }
             else if (tamEdedHavaderece < 0 && tamEdedHavaderece >= -3) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-cloud-rain"></i>`;
+                document.querySelector(".ikinciHavaFont2").innerHTML = `<i class="fa-solid fa-cloud-rain"></i>`;
             }
 
             else if (tamEdedHavaderece < -3) {
-                document.querySelector(".havaFont").innerHTML = `<i class="fa-solid fa-snowflake"></i>`;
+                document.querySelector(".ikinciHavaFont2").innerHTML = `<i class="fa-solid fa-snowflake"></i>`;
             }
         })
+        .catch(error => console.error('Error fetching timezone data:', error));
 
     function fetchTimeZoneData() {
-        timezoneKey = 'X6B3J2LJQON9';
-        let timeApiURl2 = `https://api.timezonedb.com/v2.1/get-time-zone?key=${timezoneKey}&format=json&by=position&lat=${latitude}&lng=${longitude}`;
+         const timezoneKey = 'X6B3J2LJQON9';
+        let timeApiURl3 = `https://api.timezonedb.com/v2.1/get-time-zone?key=${timezoneKey}&format=json&by=position&lat=${latitude}&lng=${longitude}`;
         // console.log(timeApiURl2);
-        fetch(timeApiURl2)
+        fetch(timeApiURl3)
             .then(response => response.json())
             .then(data => {
                 var clock2 = data.formatted;
@@ -265,7 +319,8 @@ navigator.geolocation.getCurrentPosition(function (position) {
                 document.querySelector(".ikinciSaheSaat2Div3").innerHTML = timePart2[1];
                 //   console.log(time2);
             })
-    }
-    fetchTimeZoneData();
-    setInterval(fetchTimeZoneData, 1000);
+            .catch(error => console.error('Error fetching timezone data:', error));
+        }
+        fetchTimeZoneData();
+        setInterval(fetchTimeZoneData, 60000);
 });
